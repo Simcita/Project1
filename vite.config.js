27584fc2eb756864a569d57c50ee48fa7e5f37ca
Simcita <mlisanasimamnkele10@gmail>
2025-07-31 00:31:30 +0200

    Deploy to GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/Project1/',
-  build: {
-    outDir: 'docs'
-  },
+  base: './',
   plugins: [react()],
 })
